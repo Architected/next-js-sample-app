98dba03e5c2e5b9c530c2a73d781b744aa893446
Christian Farinella <christian@architected.net>
2021-12-16 13:08:59 +1100

    Add artifacts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,18 @@
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
 
-//const projectId = process.env.INFURA_PROJECTID;
-//const accountPrivateKey = process.env.APP_WALLET_PRIVATE_KEY;
+const projectId = process.env.INFURA_PROJECTID;
+const accountPrivateKey = process.env.CONTRACT_OWNER_PRIVATE_KEY;
 
 module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
     },
-    /*mumbai: {
+    mumbai: {
       url: 'https://polygon-mumbai.infura.io/v3/' + projectId,
       accounts: [accountPrivateKey],
     },
-    mainnet: {
-      url: 'https://polygon-mainnet.infura.io/v3/' + projectId,
-      accounts: [accountPrivateKey],
-    },*/
   },
   solidity: '0.8.4',
 };
